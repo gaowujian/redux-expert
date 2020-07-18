f335a51a41d6f06421dadc66e1ead44f3c471db7
Tony Gao <gwjlshebei@gmail.com>
2020-07-18 15:07:17 +0800

    优化手写bindActionCreators

--- src/redux/bindActionCreators.js
+++ src/redux/bindActionCreators.js
@@ -1,21 +1,30 @@
+function bindActionCreator(actionCreator, dispatch) {
+  dispatch(actionCreator());
+  return;
+}
 function bindActionCreators(actionCreators, dispatch) {
   if (typeof actionCreators === "function") {
-    return function () {
-      dispatch(actionCreators());
-      return;
-    };
+    return bindActionCreator;
   }
   //   处理对象类型的 creators
   // actionCreators { add : fn(){}, minus: fn(){} }
 
   const boundActionCreators = {};
-  const actionCreatorsKeys = Object.keys(actionCreators);
+  // const actionCreatorsKeys = Object.keys(actionCreators);
 
-  actionCreatorsKeys.forEach((key) => {
-    boundActionCreators[key] = function () {
-      dispatch(actionCreators[key]());
-    };
-  });
+  // actionCreatorsKeys.forEach((key) => {
+  //   boundActionCreators[key] = function () {
+  //     dispatch(actionCreators[key]());
+  //   };
+  // });
+  for (const key in actionCreators) {
+    if (actionCreators.hasOwnProperty(key)) {
+      const actionCreator = actionCreators[key];
+      if (typeof actionCreator === "function") {
+        boundActionCreators[key] = bindActionCreator(actionCreator, dispatch);
+      }
+    }
+  }
 
   return boundActionCreators;
 }
