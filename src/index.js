72bf019c92ca72a86139b6d9d47432af005932e1
Tony Gao <gwjlshebei@gmail.com>
2020-07-18 13:32:24 +0800

    原生createStore

--- src/index.js
+++ src/index.js
@@ -1,17 +1,64 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+import { createStore } from "redux";
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+const initialState = {
+  number: 0,
+};
+
+const reducer = (state = initialState, { type }) => {
+  switch (type) {
+    case "add":
+      return { ...state, number: state.number + 1 };
+
+    case "minus":
+      return { ...state, number: state.number - 1 };
+    default:
+      return state;
+  }
+};
+
+const store = createStore(reducer);
+
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = store.getState();
+  }
+  render() {
+    return (
+      <div>
+        <h1>Counter案例</h1>
+        <p>number:{this.state.number}</p>
+        <button onClick={() => store.dispatch({ type: "add" })}>+</button>
+        <button onClick={() => store.dispatch({ type: "minus" })}>-</button>
+        <br />
+        <button
+          onClick={() => {
+            this.unsubscribe = store.subscribe(() => {
+              this.setState(store.getState());
+            });
+          }}
+        >
+          添加监听
+        </button>
+        <button
+          onClick={() => {
+            this.unsubscribe();
+          }}
+        >
+          取消监听{" "}
+        </button>
+      </div>
+    );
+  }
+  componentDidMount() {
+    const unsubscribe = store.subscribe(() => {
+      this.setState(store.getState());
+    });
+    this.unsubscribe = unsubscribe;
+  }
+}
+
+ReactDOM.render(<App />, document.getElementById("root"));
