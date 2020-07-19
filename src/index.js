c8ccfaa62cc5f8956c5e7282a7895ad3e38e886b
Tony Gao <gwjlshebei@gmail.com>
2020-07-19 22:04:17 +0800

    使用原生applymiddleware

--- src/index.js
+++ src/index.js
@@ -1,21 +1,22 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
-import { createStore } from "redux";
+import { createStore, applyMiddleware } from "redux";
 import rootReducer from "./redux/reducers";
 import * as actions from "./redux/actions/counter";
 import Counter from "./components/Counter";
 
-export const store = createStore(rootReducer);
-
-const oldDispatch = store.dispatch;
-
-store.dispatch = function () {
-  console.log(`老状态:${JSON.stringify(store.getState())}`);
-  oldDispatch(actions.add());
-  console.log(`新状态:${JSON.stringify(store.getState())}`);
+const logger = function ({ getState, dispatch }) {
+  return function (next) {
+    return function (action) {
+      console.log(`老状态:${JSON.stringify(store.getState())}`);
+      next(action);
+      console.log(`新状态:${JSON.stringify(store.getState())}`);
+    };
+  };
 };
-export default store;
+
+const store = createStore(rootReducer, applyMiddleware(logger));
 ReactDOM.render(
   <Provider store={store}>
     <Counter />
