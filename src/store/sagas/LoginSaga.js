632594fb041ad0aaaf495ee5a4c0486d39f1ec53
Tony Gao <806003097@qq.com>
2020-07-20 13:50:21 +0800

    解释take的逻辑顺序

--- src/store/sagas/LoginSaga.js
+++ src/store/sagas/LoginSaga.js
@@ -21,8 +21,9 @@ export default function* () {
     // 使用fork相当于是开启了一个子进程，并不会阻塞主要进程，登录之后，直接点击退出登录就可以执行退出操作
     const task = yield fork(login, username, password);
     console.log(task);
+    // 在没有登录的时候，去发送退出登录的请求，虽然能派发动作，但是不会被saga中间键处理，因为需要先要take到login request之后，才可能去准备take logout的
+    // redux-logger是记录所有的action的，所以他是会一直去打印的
     yield take(types.LOGOUT_REQUEST);
-
     yield put({ type: types.LOGOUT_SUCCESS });
   }
 }
