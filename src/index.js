d606da1eeb68e25448c19b316b87f38398265d2d
Tony Gao <gwjlshebei@gmail.com>
2020-08-04 21:22:27 +0800

    使用dva

--- src/index.js
+++ src/index.js
@@ -1,17 +1,65 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import dva, { connect } from "dva";
+import { Router, Route } from "dva/router";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const app = dva();
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+function delay(ms) {
+  return new Promise((res, rej) => {
+    setTimeout(() => {
+      res();
+    }, ms);
+  });
+}
+app.model({
+  namespace: "counter",
+  state: {
+    number: 20,
+  },
+  reducers: {
+    add(state) {
+      return { ...state, number: state.number + 1 };
+    },
+  },
+  effects: {
+    *asyncAdd(state, { call, put }) {
+      yield delay(1000);
+      yield put({ type: "counter/add" });
+    },
+  },
+});
+
+function Counter(props) {
+  return (
+    <div>
+      <h1>counter</h1>
+      <p>number:{props.number}</p>
+      <button
+        onClick={() => {
+          props.dispatch({ type: "counter/add" });
+        }}
+      >
+        +
+      </button>
+      <br />
+      <button
+        onClick={() => {
+          props.dispatch({ type: "counter/asyncAdd" });
+        }}
+      >
+        +
+      </button>
+    </div>
+  );
+}
+
+const ConnectedCounter = connect((state) => state.counter)(Counter);
+app.router(({ history }) => {
+  return (
+    <Router history={history}>
+      <Route path="/counter" component={ConnectedCounter} />
+    </Router>
+  );
+});
+
+app.start("#root");
