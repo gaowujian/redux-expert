6394aa5f23c2985527f58f9f291151cb7345f444
Tony Gao <806003097@qq.com>
2020-08-05 11:15:11 +0800

    实现两个counter之间的相互操作

--- src/dva/index.js
+++ src/dva/index.js
@@ -8,6 +8,8 @@ import createSagaMiddleware from "redux-saga";
 import * as sagaEffects from "redux-saga/effects";
 
 export { connect };
+
+const NAMESPACE_SEP = "/";
 export default function (opts = {}) {
   const history = opts.history || createHashHistory();
   let app = {
@@ -19,7 +21,9 @@ export default function (opts = {}) {
   };
   function model(model) {
     const prefixedModel = prefixedNamespace(model);
+    console.log(prefixedModel);
     app._models.push(prefixedModel);
+    console.log(app._models);
   }
   function router(routerConfig) {
     app._router = routerConfig;
@@ -30,7 +34,10 @@ export default function (opts = {}) {
     const sagaMiddleware = createSagaMiddleware();
     app._store = createStore(reducers, applyMiddleware(sagaMiddleware));
     const sagas = getSagas(app);
-    sagaMiddleware.run(sagas);
+    sagas.forEach((saga) => {
+      // run就是启动saga执行
+      sagaMiddleware.run(saga);
+    });
     ReactDOM.render(
       <Provider store={app._store}>{app._router()}</Provider>,
       document.querySelector(containerId)
@@ -44,14 +51,12 @@ function getReducers(app) {
   // 目标reducers是 {counter:function reducer(){},counter2:function reducer(){}}
   const reducers = {};
   const models = app._models;
-  console.log(models);
   //   models  [{namespace:counter,reducers:{},effects:{}}]
   for (const model of models) {
     reducers[model.namespace] = function (state = model.state, action) {
       // 先获取模型的所有reducers
       const model_reducers = model.reducers;
 
-      console.log(model);
       // 获取指定的reducer 等价于原来的一条switch case分支
       let reducer = model_reducers[action.type];
       if (reducer) {
@@ -64,25 +69,38 @@ function getReducers(app) {
   return combineReducers(reducers);
 }
 
-// add 变成counter/add
-function prefixedNamespace(model) {
-  let reducers = model.reducers;
-  model.reducers = Object.keys(reducers).reduce((memo, key) => {
-    const newKey = `${model.namespace}\/${key}`;
+// add 变成counter/add  还需要让
+function prefix(obj, namespace) {
+  return Object.keys(obj).reduce((memo, key) => {
+    const newKey = `${namespace}${NAMESPACE_SEP}${key}`;
     // console.log(newKey);
-    memo[newKey] = reducers[key];
+    memo[newKey] = obj[key];
     return memo;
   }, {});
+}
+
+function prefixedNamespace(model) {
+  // 支持reducers
+  if (model.reducers) {
+    model.reducers = prefix(model.reducers, model.namespace);
+  }
+  // 支持effects
+  if (model.effects) {
+    model.effects = prefix(model.effects, model.namespace);
+  }
   return model;
 }
 
-// 需要把每一个effect都转换成一个saga然后让saga中间件去监听
 function getSagas(app) {
   const sagas = [];
-  for (const model of app.models) {
+  for (const model of app._models) {
+    //需要把effects对象变成一个saga，注意是一个
+    // 这一个大的里面，每一个effects方法又有一个watcher的小saga方法
     sagas.push(function* () {
       for (const key in model.effects) {
+        // key: counter/asyncAdd     watcher也是一个saga，然后用fork使得这个saga单独执行
         const watcher = getWatcher(key, model.effects[key], model);
+        // fork可以单独开一个进程，而不会阻塞当前saga
         yield sagaEffects.fork(watcher);
       }
     });
@@ -90,6 +108,32 @@ function getSagas(app) {
   return sagas;
 }
 
-function getWatcher(params) {
-  return function* (params) {};
+//
+function getWatcher(key, effect, model) {
+  // 这边是去重写put方法 使得 effects中可以用 put({type:add})去触发 reducer中的 counter/add
+  function put(action) {
+    return sagaEffects.put({ ...action, type: prefixType(action.type, model) });
+  }
+
+  return function* () {
+    // args就是action, { call, put }
+    //key counter/asyncAdd
+    yield sagaEffects.takeEvery(key, function* (...args) {
+      // 这边是去重写put方法 使得 effects中可以用 put({type:add})去触发 reducer中的 counter/add
+      yield effect(...args, { ...sagaEffects, put: put });
+    });
+  };
+}
+
+function prefixType(type, model) {
+  // 这边是做了一个简单的判断，如果没有命名空间，就需要加上，如果有了就不需要加
+  if (type.indexOf(NAMESPACE_SEP) === -1) {
+    return `${model.namespace}${NAMESPACE_SEP}${type}`;
+  } else {
+    // 添加一个警告，在put的时候不需要加namespace，但是不会去阻止动作
+    console.error(
+      `Warning: [sagaEffects.put] ${type} should not be prefixed with namespace ${model.namespace}`
+    );
+  }
+  return type;
 }
--- src/index.js
+++ src/index.js
@@ -12,7 +12,7 @@ function delay(ms) {
   });
 }
 app.model({
-  namespace: "counter",
+  namespace: "counter1",
   state: {
     number: 20,
   },
@@ -20,50 +20,132 @@ app.model({
     add(state) {
       return { ...state, number: state.number + 1 };
     },
+    minus(state) {
+      return { ...state, number: state.number - 1 };
+    },
   },
   effects: {
     // 原理就是去监听 counter/asyncAdd的方法, 监听到了这个saga
     // TakeEvery("counter/asyncAdd",*asyncAdd(action,effects))
-    *asyncAdd(state, { call, put }) {
-      console.log("11");
+    *asyncAdd(action, { call, put }) {
       yield delay(1000);
       yield put({ type: "add" });
+      // 也可以去派发别的effects，而不一定是reducers
+      // yield put({ type: "asyncMinus" });
+    },
+    *asyncMinus(action, { call, put }) {
+      yield delay(1000);
+      yield put({ type: "minus" });
     },
   },
 });
 
-function Counter(props) {
+app.model({
+  namespace: "counter2",
+  state: {
+    number: 10,
+  },
+  reducers: {
+    add(state) {
+      return { ...state, number: state.number + 1 };
+    },
+    minus(state) {
+      return { ...state, number: state.number - 1 };
+    },
+  },
+  effects: {
+    // 原理就是去监听 counter/asyncAdd的方法, 监听到了这个saga
+    // TakeEvery("counter/asyncAdd",*asyncAdd(action,effects))
+    *asyncAdd(action, { call, put }) {
+      yield delay(1000);
+      yield put({ type: "add" });
+      // 也可以去派发别的effects，而不一定是reducers
+      // yield put({ type: "asyncMinus" });
+    },
+    *asyncMinus(action, { call, put }) {
+      yield delay(1000);
+      yield put({ type: "minus" });
+    },
+  },
+});
+
+function Counter1(props) {
   return (
     <div>
       <h1>counter</h1>
       <p>number:{props.number}</p>
       <button
         onClick={() => {
-          props.dispatch({ type: "counter/add" });
+          props.dispatch({ type: "counter1/add" });
         }}
       >
         +
       </button>
-      <br />
+
       <button
         onClick={() => {
-          props.dispatch({ type: "counter/asyncAdd" });
+          props.dispatch({ type: "counter1/asyncAdd" });
         }}
       >
         +
+      </button>
+      <button
+        onClick={() => {
+          props.dispatch({ type: "counter1/asyncMinus" });
+        }}
+      >
+        -
       </button>
     </div>
   );
 }
 
-const ConnectedCounter = connect((state) => state.counter)(Counter);
+const ConnectedCounter1 = connect((state) => state.counter1)(Counter1);
+
+function Counter2(props) {
+  return (
+    <div>
+      <h1>counter</h1>
+      <p>number:{props.number}</p>
+      <button
+        onClick={() => {
+          props.dispatch({ type: "counter1/add" });
+        }}
+      >
+        +
+      </button>
+
+      <button
+        onClick={() => {
+          props.dispatch({ type: "counter1/asyncAdd" });
+        }}
+      >
+        +
+      </button>
+      <button
+        onClick={() => {
+          props.dispatch({ type: "counter2/asyncMinus" });
+        }}
+      >
+        -
+      </button>
+    </div>
+  );
+}
+
+const ConnectedCounter2 = connect((state) => state.counter2)(Counter2);
 app.router(() => {
   // return (
   //   <Router history={history}>
   //     <Route path="/counter" component={ConnectedCounter} />
   //   </Router>
   // );
-  return <ConnectedCounter />;
+  return (
+    <div>
+      <ConnectedCounter1 />
+      <ConnectedCounter2 />
+    </div>
+  );
 });
 
 app.start("#root");
