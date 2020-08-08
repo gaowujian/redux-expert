8985fc29fa27083bf3c67d4890ef560b032b929d
Tony Gao <gwjlshebei@gmail.com>
2020-08-08 10:49:29 +0800

    实现connectRouer

---
+++ src/connected-react-router/connectRouter.js
@@ -0,0 +1,18 @@
+import { LOCATION_CHANGE } from "./constant";
+
+export default function (history) {
+  const initialState = {
+    action: history.action,
+    location: history.location,
+  };
+
+  return (state = initialState, { type, payload }) => {
+    switch (type) {
+      case LOCATION_CHANGE: {
+        return { ...payload };
+      }
+      default:
+        return state;
+    }
+  };
+}
