96d68769b79282ecccdb1559225d4db6b0a91ec7
Tony Gao <gwjlshebei@gmail.com>
2020-08-08 11:21:55 +0800

    使用redux-persist

--- src/index.js
+++ src/index.js
@@ -5,24 +5,27 @@ import { Provider } from "react-redux";
 import { NavLink, Route } from "react-router-dom";
 import Counter from "./components/Counter";
 import Home from "./components/Home";
-import store from "./redux";
+import { store, persistor } from "./redux";
 import history from "./redux/history";
 import "./style.css";
+import { PersistGate } from "redux-persist/integration/react";
 
 class App extends Component {
   render() {
     return (
       <Provider store={store}>
-        <ConnectedRouter history={history}>
-          <NavLink to="/home" className="link">
-            home
-          </NavLink>
-          <NavLink to="/counter" className="link">
-            counter
-          </NavLink>
-          <Route path="/home" exact component={Home}></Route>
-          <Route path="/counter" exact component={Counter}></Route>
-        </ConnectedRouter>
+        <PersistGate loading={null} persistor={persistor}>
+          <ConnectedRouter history={history}>
+            <NavLink to="/home" className="link">
+              home
+            </NavLink>
+            <NavLink to="/counter" className="link">
+              counter
+            </NavLink>
+            <Route path="/home" exact component={Home}></Route>
+            <Route path="/counter" exact component={Counter}></Route>
+          </ConnectedRouter>
+        </PersistGate>
       </Provider>
     );
   }
--- src/redux/index.js
+++ src/redux/index.js
@@ -2,7 +2,21 @@ import { createStore, applyMiddleware } from "redux";
 import rootReducer from "./reducers";
 import { routerMiddleware } from "connected-react-router";
 import history from "./history";
-export default createStore(
-  rootReducer,
+import { persistReducer, persistStore } from "redux-persist";
+import storage from "redux-persist/lib/storage"; // defaults to localStorage for web
+
+const persistConfig = {
+  key: "root",
+  storage,
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+const store = createStore(
+  persistedReducer,
   applyMiddleware(routerMiddleware(history))
 );
+
+let persistor = persistStore(store);
+
+export { store, persistor };
